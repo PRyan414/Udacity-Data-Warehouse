02f9878033f5b6dd709b53a170943442f2c28073
Peter Ryan <87825966+PRyan414@users.noreply.github.com>
2024-10-03 21:13:34 -0500

    Update create_tables.py
    Correct docstrings and formatting.

--- create_tables.py
+++ create_tables.py
@@ -1,4 +1,4 @@
-# Code with functions to drop all tables in a given list, and also create tables from a given list.
+"""Code with functions to drop all tables in a given list, and also create tables from a given list."""
 import configparser
 import psycopg2
 import boto3
@@ -9,7 +9,6 @@ from sql_queries import create_table_queries, drop_table_queries
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
-# Config file information needed to connect to the Redshift cluster already created.
 DWH_IAM_ROLE_NAME = config.get("DWH", "DWH_IAM_ROLE_NAME")
 CLUSTER_DB_NAME = config.get("CLUSTER", "dbname")
 CLUSTER_DB_USER = config.get("CLUSTER", "user")
@@ -17,35 +16,31 @@ CLUSTER_DB_PASSWORD = config.get("CLUSTER", "password")
 CLUSTER_DB_PORT = config.get("CLUSTER", "port")
 CLUSTER_DB_ENDPOINT = config.get("CLUSTER", "host")
 
-# Function to drop tables imported in the list "drop_table_queries" from sql_queries.py
-# For each query/table in the list, code to drop each table is run.
 def drop_tables(cur, conn):
+    """Function to drop tables from database if they are found to exist."""
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 def create_tables(cur, conn):
+        """Function creates tables for database as defined in sql_queries.py."""
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 def main():
+    """Executes functions drop_tables followed by crete_tables as precursors to etl.py."""
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    # Connect to Redshift and set up tables
-    # Connection string to use in connection command.
     conn_string = f"dbname={CLUSTER_DB_NAME} user={CLUSTER_DB_USER} password={CLUSTER_DB_PASSWORD} host={CLUSTER_DB_ENDPOINT} port={CLUSTER_DB_PORT}"
 
-    # Create connection and establish a cursor to execute commands on the luster
     conn = psycopg2.connect(conn_string)
     cur = conn.cursor()
 
-    # Run each of the functions above, sequentially.
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
-    # Close the cursor connection.
     conn.close()
 
 
